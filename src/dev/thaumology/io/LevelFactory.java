9343c783a5ea84a2f431115ac560fce5cd1dee81
ReBuilders101 <rebuilders101@gmail.com>
2018-11-13 18:04:33 +0100

    Level file io
    I guess I have to do this part before I can test the node builder

---
+++ src/dev/thaumology/io/LevelFactory.java
@@ -0,0 +1,14 @@
+package dev.thaumology.io;
+
+import org.jnbt.Tag;
+
+import dev.thaumology.world.World;
+
+public final class LevelFactory {
+	private LevelFactory(){}
+	
+	public static World createWorld(Tag tag) {
+		return null;
+	}
+	
+}
---
+++ src/dev/thaumology/io/LevelFormatException.java
@@ -0,0 +1,16 @@
+package dev.thaumology.io;
+
+public class LevelFormatException extends Exception{
+	private static final long serialVersionUID = -2691212623128254021L;
+	
+	public LevelFormatException() {
+		super("No Error Message");
+	}
+
+	public LevelFormatException(String message) {
+		super(message);
+	}
+
+	
+
+}
